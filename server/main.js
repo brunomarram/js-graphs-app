ce15a9bad1657cd1b3a794b90483e1a4860a5239
Admin <>
2018-11-06 11:34:57 -0200

    Cria função para caminho mínimo

--- server/main.js
+++ server/main.js
@@ -3,28 +3,31 @@ import Graphs from "meteor/brunomarram:js-graphs";
 
 const fs = require("fs");
 
+const _readGraph = (file) => {
+    const graph = new Graphs.Graphs("Grafo 01");
+    const textFile = Assets.getText(file);
+    const lines = textFile.split("\n");
+    let size = lines[0];
+    for (let i = 1; i <= parseInt(size); i++) graph.addNode(i.toString(), i);
+    lines.shift();
+
+    lines.forEach((line) => {
+        line = line.split(" ");
+        const source = graph.getNode(line[0]),
+            target = graph.getNode(line[1]),
+            value = parseFloat(line[2]);
+        graph.addEdge(source, target, value);
+    });
+
+    return graph;
+};
+
 Meteor.startup(() => {
     try {
-        const graph = new Graphs.Graphs("Grafo 01");
-        const textFile = Assets.getText("grafos/grafo1.txt");
-        const lines = textFile.split("\n");
-        let nodes = lines[0];
-        for (let i = 1; i <= parseInt(nodes); i++)
-            graph.addNode(i.toString(), i);
-        lines.shift();
-
-        lines.forEach((line) => {
-            line = line.split(" ");
-            const source = graph.getNode(line[0]),
-                target = graph.getNode(line[1]),
-                value = parseFloat(line[2]);
-            graph.addEdge(source, target, value);
-        });
-
-        const node = graph.getNode("5");
-
-        //console.log(graph.getNeighbors(node));
-        console.log(graph.isEurelian());
+        const graph = _readGraph("grafos/grafo1.txt");
+        const source = graph.getNode("1");
+        const target = graph.getNode("4");
+        console.log(graph.getMinimumPath(source, target));
     } catch (e) {
         console.log("Error:", e.stack);
     }
